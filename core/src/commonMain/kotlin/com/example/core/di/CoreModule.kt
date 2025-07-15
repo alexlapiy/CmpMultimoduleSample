7a48254f8f7e47acc98e3fcb086cd0b33d329126
Aleksandr <alekslapiy@gmail.com>
2025-07-15 22:38:50 +0300

    ignoreUnknownKeys to fix response

--- core/src/commonMain/kotlin/com/example/core/di/CoreModule.kt
+++ core/src/commonMain/kotlin/com/example/core/di/CoreModule.kt
@@ -7,13 +7,14 @@ import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.dsl.module
 
 val coreModule = module {
     single {
         HttpClient {
             install(ContentNegotiation) {
-                json()
+                json(Json { ignoreUnknownKeys = true })
             }
             install(Logging) {
                 logger = Logger.DEFAULT
