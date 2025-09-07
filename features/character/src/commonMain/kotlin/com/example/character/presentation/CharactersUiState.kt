4d4665a9ff761332dcfa3fe1d87809e01d82ce6f
Aleksandr <alekslapiy@gmail.com>
2025-09-07 19:48:16 +0300

    minor fix

--- features/character/src/commonMain/kotlin/com/example/character/presentation/CharactersUiState.kt
+++ features/character/src/commonMain/kotlin/com/example/character/presentation/CharactersUiState.kt
@@ -2,7 +2,7 @@ package com.example.character.presentation
 
 import com.example.character.models.items.CharacterItemUi
 
-interface CharactersUiState {
+sealed interface CharactersUiState {
     data object Loading : CharactersUiState
     data class Success(val characters: List<CharacterItemUi>) : CharactersUiState
     data class Error(val message: String) : CharactersUiState
